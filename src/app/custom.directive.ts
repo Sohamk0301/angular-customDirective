65a4aacec560720ac4b3db6fdfcf9f96241c7037
Sohamk0301 <100216902+Sohamk0301@users.noreply.github.com>
2023-03-29 15:21:00 +0530

    2 files modified

--- src/app/custom.directive.ts
+++ src/app/custom.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appCustom]'
 })
 export class CustomDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { 
+    elem.nativeElement.style.color="green";
+    elem.nativeElement.style.background="orange";
+  }
 
 }
